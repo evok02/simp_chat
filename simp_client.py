3bb8c60491e56c95106c1a963163fa042fa836fe
k0zachok <okseniukvlas@gmail.com>
2024-12-14 21:17:39 +0100

    chat implemented, but improvement required

--- simp_client.py
+++ simp_client.py
@@ -1,103 +1,35 @@
 import socket
-import struct
+import threading
 
-# Constants
-PORT_DAEMON = 7777
-PORT_CLIENT = 7778
-HEADER_FORMAT = "!BB32sI"  # Type, Operation, User, Length
-BUFFER_SIZE = 1024
-
-# SIMP Datagram Types and Operations
-TYPE_CONTROL = 0x01
-TYPE_CHAT = 0x02
-OP_ERR = 0x01
-OP_SYN = 0x02
-OP_ACK = 0x04
-OP_FIN = 0x08
-
-class SIMPClient:
-    def __init__(self, daemon_ip):
-        self.daemon_ip = daemon_ip
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(("", PORT_CLIENT))
+class UdpClient:
+    def __init__(self, daemon_host):
+        self.daemon_address = (daemon_host, 7778)  # Connect to daemon's client port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.username = None
-        self.chatting = False
 
-    def run(self):
+    def send_message(self, message):
+        """Send a message to the daemon."""
+        self.sock.sendto(message.encode(), self.daemon_address)
+
+    def receive_messages(self):
+        """Listen for incoming messages from the daemon."""
+        while True:
+            data, _ = self.sock.recvfrom(1024)
+            print(data.decode())  # Print messages received from the daemon
+
+    def start(self):
+        """Start the client."""
         self.username = input("Enter your username: ")
-        self.connect()
+        self.send_message(self.username)  # Send the username to the daemon
+
+        print("\nYou can now send messages. Type your message and press Enter.")
+        threading.Thread(target=self.receive_messages, daemon=True).start()
+
         while True:
-            command = input("Enter command (start, wait, quit): ")
-            if command == "start":
-                target_ip = input("Enter the IP address of the user to chat with: ")
-                self.send_syn(target_ip)
-            elif command == "wait":
-                print("Waiting for chat requests...")
-                self.wait_for_requests()
-            elif command == "quit":
-                self.quit()
-                break
-
-    def connect(self):
-        print(f"Connecting to daemon at {self.daemon_ip}:{PORT_DAEMON}")
-
-    def send_syn(self, target_ip):
-        header = struct.pack(HEADER_FORMAT, TYPE_CONTROL, OP_SYN, self.username.encode('ascii'), len(target_ip))
-        self.socket.sendto(header + target_ip.encode('ascii'), (self.daemon_ip, PORT_DAEMON))
-        print(f"Sent chat request to {target_ip}")
-
-    def wait_for_requests(self):
-        while True:
-            data, addr = self.socket.recvfrom(BUFFER_SIZE)
-            self.handle_response(data, addr)
-
-    def handle_response(self, data, addr):
-        header = data[:38]
-        payload = data[38:]
-        msg_type, operation, user, length = struct.unpack(HEADER_FORMAT, header)
-        message = payload.decode('ascii')
-        if operation == OP_ERR:
-            print(f"Error from daemon: {message}")
-        elif operation == OP_SYN:
-            print(f"Chat request from {message}")
-            response = input("Accept chat request? (yes/no): ")
-            if response == "yes":
-                self.send_ack(addr)
-                self.chatting = True
-                self.chat(addr)
-            else:
-                self.send_fin(addr)
-
-    def send_ack(self, addr):
-        header = struct.pack(HEADER_FORMAT, TYPE_CONTROL, OP_ACK, self.username.encode('ascii'), 0)
-        self.socket.sendto(header, addr)
-        print(f"Accepted chat request from {addr}")
-
-    def send_fin(self, addr):
-        header = struct.pack(HEADER_FORMAT, TYPE_CONTROL, OP_FIN, self.username.encode('ascii'), 0)
-        self.socket.sendto(header, addr)
-        print(f"Declined chat request from {addr}")
-
-    def chat(self, addr):
-        print("Chatting... Type your messages below.")
-        while self.chatting:
             message = input()
-            if message == "quit":
-                self.send_fin(addr)
-                self.chatting = False
-            else:
-                header = struct.pack(HEADER_FORMAT, TYPE_CHAT, 0, self.username.encode('ascii'), len(message))
-                self.socket.sendto(header + message.encode('ascii'), addr)
+            self.send_message(message)
 
-    def quit(self):
-        print("Disconnecting from daemon")
-        self.socket.close()
-
-# Example usage
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python simp_client.py <daemon_ip>")
-        sys.exit(1)
-    client = SIMPClient(sys.argv[1])
-    client.run()
+    daemon_host = input("Enter the IP address of the daemon to connect to: ")
+    client = UdpClient(daemon_host)
+    client.start()
--- simp_daemon.py
+++ simp_daemon.py
@@ -1,84 +1,110 @@
 import socket
-import struct
 import threading
 
-# Constants
-PORT_DAEMON = 7777
-PORT_CLIENT = 7778
-HEADER_FORMAT = "!BB32sI"  # Type, Operation, User, Length
-BUFFER_SIZE = 1024
+class UdpDaemon:
+    def __init__(self, host):
+        self.daemon_address = (host, 7777)  # Daemon-to-daemon communication on port 7777
+        self.client_address = None  # To store the connected client address (port 7778)
+        self.daemons = {}  # Known daemons (address: (host, port))
+        self.client_username = None  # Username of the connected client
+        
+        # Daemon socket for communication with other daemons
+        self.daemon_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.daemon_sock.bind(self.daemon_address)
+        
+        # Client socket for communication with client
+        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_sock.bind((host, 7778))
 
-# SIMP Datagram Types and Operations
-TYPE_CONTROL = 0x01
-TYPE_CHAT = 0x02
-OP_ERR = 0x01
-OP_SYN = 0x02
-OP_ACK = 0x04
-OP_FIN = 0x08
+        # Predefined daemon addresses
+        self.default_daemons = []
 
-class SIMPDaemon:
-    def __init__(self, ip):
-        self.ip = ip
-        self.clients = {}  # {address: username}
-        self.pending_requests = {}  # {client_address: (requesting_address, requesting_username)}
-        self.active_chats = {}  # {client_address: partner_address}
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((ip, PORT_DAEMON))
-        print(f"Daemon running on {ip}:{PORT_DAEMON}")
+    def send_message_to_daemons(self, username, message):
+        """Send message with username to all known daemons."""
+        formatted_message = f"{username}: {message}"
+        for address in self.daemons.values():
+            self.daemon_sock.sendto(formatted_message.encode(), address)
+
+    def forward_to_client(self, message):
+        """Forward a message to the connected client."""
+        if self.client_address:
+            self.client_sock.sendto(message.encode(), self.client_address)
+
+    def handle_daemon_messages(self):
+        """Handle messages from other daemons."""
+        while True:
+            data, address = self.daemon_sock.recvfrom(1024)
+            
+            # Add sender to known daemons if new
+            if address not in self.daemons.values() and address != self.daemon_address:
+                self.daemons[address] = address
+                print(f"Added new daemon: {address}")
+
+            # Forward the message to the client
+            self.forward_to_client(data.decode())
+
+    def handle_client_messages(self):
+        """Handle messages from the connected client."""
+        while True:
+            data, address = self.client_sock.recvfrom(1024)
+            
+            # If the client is not yet registered, register it
+            if self.client_address is None:
+                self.client_address = address
+                print(f"Client connected from {self.client_address}")
+
+            # If username is not yet set, interpret the first message as the username
+            if self.client_username is None:
+                self.client_username = data.decode()
+                print(f"Client username set to {self.client_username}")
+
+                # Broadcast that this user has joined
+                self.send_message_to_daemons(self.client_username, "has joined the chat!")
+                self.forward_to_client(f"Welcome, {self.client_username}!")
+                continue
+
+            # Broadcast the client's message to all other daemons
+            print(f"From client: {data.decode()}")
+            self.send_message_to_daemons(self.client_username, data.decode())
+
+
+    def discover_daemons(self):
+        """Send a discovery message to all default daemons."""
+        discovery_message = "DISCOVER"
+        for daemon in self.default_daemons:
+            if daemon != self.daemon_address:  # Exclude self from discovery
+                self.daemon_sock.sendto(discovery_message.encode(), daemon)
 
     def start(self):
+        """Start the daemon to handle client and daemon messages."""
+        print(f"Daemon started. Listening on {self.daemon_address} for daemons.")
+        print("Listening on port 7778 for client connections.")
+
+        # Send discovery messages
+        self.discover_daemons()
+
+        # Start separate threads for daemon and client communication
+        threading.Thread(target=self.handle_daemon_messages, daemon=True).start()
+        threading.Thread(target=self.handle_client_messages, daemon=True).start()
+
         while True:
-            data, addr = self.socket.recvfrom(BUFFER_SIZE)
-            print(f"Received connection from {addr}")
-            self.handle_client(data, addr)
+            pass
 
-    def handle_client(self, data, addr):
-        header = data[:38]
-        payload = data[38:]
-        msg_type, operation, user, length = struct.unpack(HEADER_FORMAT, header)
-        username = user.decode('ascii').strip('\x00')
-
-        if msg_type == TYPE_CONTROL:
-            if operation == OP_SYN:
-                self.handle_syn(addr, username, payload.decode('ascii'))
-            elif operation == OP_FIN:
-                self.handle_fin(addr)
-        elif msg_type == TYPE_CHAT:
-            self.route_message(addr, payload.decode('ascii'))
-
-    def handle_syn(self, addr, username, target_ip):
-        if addr in self.active_chats:
-            self.send_error(addr, "Already in a chat")
-        elif addr in self.pending_requests:
-            self.send_error(addr, "Already has a pending request")
-        else:
-            self.pending_requests[addr] = (target_ip, username)
-            print(f"Chat request from {username} at {addr} for {target_ip}")
-            # Forward the chat request to the target client
-            target_addr = (target_ip, PORT_CLIENT)
-            header = struct.pack(HEADER_FORMAT, TYPE_CONTROL, OP_SYN, username.encode('ascii'), 0)
-            self.socket.sendto(header, target_addr)
-
-    def handle_fin(self, addr):
-        if addr in self.active_chats:
-            partner = self.active_chats.pop(addr)
-            self.active_chats.pop(partner, None)
-            print(f"Chat ended between {addr} and {partner}")
-
-    def route_message(self, addr, message):
-        if addr in self.active_chats:
-            partner = self.active_chats[addr]
-            self.socket.sendto(message.encode('ascii'), partner)
-        else:
-            self.send_error(addr, "No active chat")
-
-    def send_error(self, addr, message):
-        print(f"Sending error to {addr}: {message}")
-        error_msg = struct.pack(HEADER_FORMAT, TYPE_CONTROL, OP_ERR, b'Error', len(message))
-        self.socket.sendto(error_msg + message.encode('ascii'), addr)
-
-# Example usage
 if __name__ == "__main__":
-    daemon = SIMPDaemon("127.0.0.1")
-    daemon_thread = threading.Thread(target=daemon.start)
-    daemon_thread.start()
+    host = input("Enter daemon host (IP address this daemon will run on, e.g., 127.0.0.1): ")
+    daemon = UdpDaemon(host)
+
+    print("\n=== Configure Other Known Daemons ===")
+    while True:
+        other_host = input("Enter another daemon IP (or press Enter to finish): ")
+        if not other_host:
+            break
+        daemon.default_daemons.append((other_host, 7777))
+
+    print(f"\nKnown daemons: {daemon.default_daemons}\n")
+    daemon.start()
+
+
+
+#192.168.1.255
+#192.168.1.20
